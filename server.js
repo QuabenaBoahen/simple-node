36cf493d10d1932397ea968d55dc25f34516ed9d
quabenaboahen <boahendeycarter@gmail.com>
2021-12-15 14:01:29 -0500

    retrieved set env variable from travis ci in server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+const favorite_series = process.en.FAVORITE_SERIES;
+
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function main() {
   while(true) {
-    console.log('Containers rule!');
+    console.log(`No series beats $favorite_series`);
     await sleep(5000);
   }
 }
